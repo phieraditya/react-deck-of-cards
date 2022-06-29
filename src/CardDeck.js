39e5d82ac7d93c72022c2f967ef37d18bddcef88
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-06-29 14:44:50 +0700

    get a deck id

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 class CardDeck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { deckID: '' };
+  }
+  componentDidMount() {
+    let urlDeck = 'https://deckofcardsapi.com/api/deck/new/shuffle';
+    // let urlCard = 'https://deckofcardsapi.com/api/deck/${deck_id}/draw/';
+    axios.get(urlDeck).then((res) => {
+      this.setState({ deckID: res.data.deck_id });
+    });
+  }
   render() {
     return (
       <div>
