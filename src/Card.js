506bc95bdaa80b568d0bf92cbb62d4318be3e2d6
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-01 16:24:33 +0700

    refactor random transform to the constructor
    (a more appropriate pattern)
    
    add it to the object itself
    as this._transform
    (hence can be called in render)
    
    note: because it is a one-time-only, it does not make sense to put in as state or props

--- src/Card.js
+++ src/Card.js
@@ -4,24 +4,19 @@ import './Card.css';
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      transform: '',
-    };
-  }
-  componentDidMount() {
     let angle = Math.random() * 90 - 45;
     let xPos = Math.random() * 40 - 20;
     let yPos = Math.random() * 40 - 20;
-    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
-    this.setState({ transform: transform });
+    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
   }
+
   render() {
     return (
       <img
         className="Card"
         src={this.props.image}
         alt={this.props.name}
-        style={{ transform: this.state.transform }}
+        style={{ transform: this._transform }}
       />
     );
   }
