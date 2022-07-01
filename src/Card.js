1007709c55d4d2e2a48665a78048fbb286c7d5c0
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-01 16:13:21 +0700

    refactor random transform from render to componentDidMount
    prevent repeat generating of random transform of the same card

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import './Card.css';
 
 class Card extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transform: '',
+    };
+  }
+  componentDidMount() {
     let angle = Math.random() * 90 - 45;
     let xPos = Math.random() * 40 - 20;
     let yPos = Math.random() * 40 - 20;
     let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+    this.setState({ transform: transform });
+  }
+  render() {
     return (
       <img
         className="Card"
         src={this.props.image}
         alt={this.props.name}
-        style={{ transform: transform }}
+        style={{ transform: this.state.transform }}
       />
     );
   }
