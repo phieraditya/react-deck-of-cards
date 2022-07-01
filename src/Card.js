4a9834b3a8a66ff154e2e75265db4ef6d2687149
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-01 16:09:23 +0700

    randomly transforming cards

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,17 @@ import './Card.css';
 
 class Card extends Component {
   render() {
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
     return (
-      <img className="Card" src={this.props.image} alt={this.props.name} />
+      <img
+        className="Card"
+        src={this.props.image}
+        alt={this.props.name}
+        style={{ transform: transform }}
+      />
     );
   }
 }
